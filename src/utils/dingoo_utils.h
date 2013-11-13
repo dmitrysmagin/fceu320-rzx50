5cefe066ebec304e73635b8e45796f8a7192fe88
Dmitry Smagin <exmortis@yandex.ru>
2013-11-13 13:33:28 +0400

    Remove legacy dingoo utils

--- src/utils/dingoo_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef _dingoo_utils_h
-#define _dingoo_utils_h
-
-void set_dingoo_cpu(int clockspeed);
-int  get_dingoo_batt_level();
-int  get_dingoo_lcd_backlight();
-void set_dingoo_lcd_backlight(int bk);
-#endif
